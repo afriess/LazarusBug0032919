a21d0776a32cb5340ceae5d25723bd513eee61bd
Andreas <andi@andifriess.com>
2018-02-17 10:10:58 +0100

    Extend the sample with the Interfeced components minor: remove unused files

--- OITestBasic/GuiOITest.lpr
+++ OITestBasic/GuiOITest.lpr
@@ -7,7 +7,7 @@ uses
   cthreads,
   {$ENDIF}{$ENDIF}
   Interfaces, // this includes the LCL widgetset
-  Forms, uOITest
+  Forms, uOITest, IntfComp
   { you can add units after this };
 
 {$R *.res}
--- OITestBasic/uoitest.pas
+++ OITestBasic/uoitest.pas
@@ -11,16 +11,18 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
-  PropEdits, ObjectInspector, PropEditUtils;
+  PropEdits, ObjectInspector, PropEditUtils, IntfComp;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
-    BuCreate: TButton;
+    BuCreateFormNormal: TButton;
     BuFree: TButton;
-    procedure BuCreateClick(Sender: TObject);
+    BuCreateFormIntf: TButton;
+    procedure BuCreateFormIntfClick(Sender: TObject);
+    procedure BuCreateFormNormalClick(Sender: TObject);
     procedure BuFreeClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
@@ -30,10 +32,6 @@ type
     ThePropertyEditorHook: TPropertyEditorHook;
     // Forms and components for the Test
     ATestForm : TForm;
-    ATestPanel : TPanel;
-    ATestPanelSub : TPanel;
-    ATestPanelSubSub : TPanel;
-
     procedure FreeUsedObjects;
   public
 
@@ -49,10 +47,16 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.BuCreateClick(Sender: TObject);
+procedure TForm1.BuCreateFormNormalClick(Sender: TObject);
+var
+  // First Test with Panel only
+  ATestPanel : TPanel;
+  ATestPanelSub : TPanel;
+  ATestPanelSubSub : TPanel;
 begin
-  BuCreate.Enabled:= false;
-  BuFree.Enabled:= not BuCreate.Enabled;
+  BuCreateFormNormal.Enabled:= false;
+  BuCreateFormIntf.Enabled:= False;
+  BuFree.Enabled:= not (BuCreateFormNormal.Enabled or BuCreateFormIntf.Enabled);
 
   DUT := TObjectInspectorDlg.Create(nil);
   DUT.Caption:= 'My ObjectInspector';
@@ -90,18 +94,60 @@ begin
   DUT.Show;
 end;
 
+procedure TForm1.BuCreateFormIntfClick(Sender: TObject);
+var
+  // Second Test with the Componnets with Interface
+  ATestIntf : TIntfComp;       // This componnets can hold a component with interface
+  ATestHasIntf : TCompHasIntf; // This component has an Interface
+begin
+  begin
+    BuCreateFormNormal.Enabled:= false;
+    BuCreateFormIntf.Enabled:= False;
+    BuFree.Enabled:= not (BuCreateFormNormal.Enabled or BuCreateFormIntf.Enabled);
+
+    DUT := TObjectInspectorDlg.Create(nil);
+    DUT.Caption:= 'My ObjectInspector';
+
+    // create the PropertyEditorHook (the interface to the properties)
+    ThePropertyEditorHook:=TPropertyEditorHook.Create(DUT);
+    DUT.PropertyEditorHook:=ThePropertyEditorHook;
+
+    // Create components
+    ATestForm :=  TForm.Create(nil);
+    ATestForm.Name:= 'Main';
+
+    ATestIntf := TIntfComp.Create(ATestForm);       // This componnets can hold a component with interface
+    ATestIntf.Name := 'ATestIntf';
+    ATestHasIntf := TCompHasIntf.Create(ATestForm); // This component has an Interface
+    ATestHasIntf.Name:= 'ATestHasIntf';
+    ATestIntf.ObjectHasInterface := ATestHasIntf;
+    ATestIntf.GetParentComponent;
+    ThePropertyEditorHook.LookupRoot:=ATestForm;
+
+    PerList:=TPersistentSelectionList.Create;
+    PerList.Add(ATestForm);
+    DUT.Selection:=PerList;
+    PerList.Free;
+
+    DUT.Show;
+  end;
+
+end;
+
 procedure TForm1.BuFreeClick(Sender: TObject);
 begin
-  BuCreate.Enabled:=true;
-  BuFree.Enabled:= not BuCreate.Enabled;
   FreeUsedObjects;
+  BuCreateFormNormal.Enabled:= true;
+  BuCreateFormIntf.Enabled:= true;
+  BuFree.Enabled:= not (BuCreateFormNormal.Enabled OR BuCreateFormIntf.Enabled);
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   DUT:= nil;
-  BuCreate.Enabled:= true;
-  BuFree.Enabled:= not BuCreate.Enabled;
+  BuCreateFormNormal.Enabled:= true;
+  BuCreateFormIntf.Enabled:= true;
+  BuFree.Enabled:= not (BuCreateFormNormal.Enabled OR BuCreateFormIntf.Enabled);
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
